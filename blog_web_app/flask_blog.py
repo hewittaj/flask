75e43fbe93816a6af05fa708944ecd34cbe91239
Alex Hewitt <hewittaj@outlook.com>
2022-02-24 10:56:11 -0600

    finished part 2 of tutorial
    https://www.youtube.com/watch?v=QnDWIZuWYW0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=2

--- blog_web_app/flask_blog.py
+++ blog_web_app/flask_blog.py
@@ -1,6 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+from distutils.log import debug
+from flask import Flask, render_template, url_for
+app = Flask(__name__, template_folder="templates")
+
+
+posts = [
+    {
+    'author': 'Alex Hewitt',
+    'title': 'Creating a blog',
+    'content': 'I created a blog.',
+    'date_posted': 'February 24, 2022'
+    },
+    {
+    'author': 'Kristen Hewitt',
+    'title': 'Reading a blog',
+    'content': 'I read a blog.',
+    'date_posted': 'February 24, 2022'
+    }
+]
 
 @app.route("/")
-def hello():
-    return "Hello"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+"""Help
+Instead of typing 'flask run' into terminal we can just run the python file and it will run the code
+"""
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ blog_web_app/installs.md
@@ -0,0 +1,11 @@
+# Installs
+`pip install flask`
+
+# Commands
+- `export FLASK_APP=flask_blog.py` : Environment variable that enables us to just type "flask run" to initialize local server
+- `export FLASK_DEBUG=1` - turns on live server reload
+*Running the environment variables is necessary every time you reload terminal. Use the code below to not have to enter environment variables each time.*
+```
+if __name__ = '__main__': 
+    app.run(debug=True)
+```
